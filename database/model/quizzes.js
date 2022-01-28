5bba2029ff5a60cd9ebe15806129b84c94a42f4a
đức khiêm <66940340+maiduckhiem@users.noreply.github.com>
2022-01-29 00:07:00 +0700

    Api

--- database/model/quizzes.js
+++ database/model/quizzes.js
@@ -5,6 +5,25 @@ const quizzesSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  backgroundImage: {
+    type: String,
+    required: true,
+  },
+  coverImage: {
+    type: String,
+    required: true,
+  },
+  music: {
+    type: String,
+    required: true,
+  },
+  needLogin: {
+    type: Boolean,
+  },
+  numberOfPlayer: {
+    type: Number,
+    required: true,
+  },
 });
 
 mongoose.model('Quizzes', quizzesSchema);
