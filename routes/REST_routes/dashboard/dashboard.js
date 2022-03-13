889cc23214da9465b2b60c3598631b3f0209bac3
laquangduc <59763734+laquangduc@users.noreply.github.com>
2022-03-13 22:08:07 +0700

    update-dashboard

--- routes/REST_routes/dashboard/dashboard.js
+++ routes/REST_routes/dashboard/dashboard.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const init = express.Router();
+const passport = require('passport');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
 init.get('/', async function (req, res) {
   try {
     const { start, end } = req.query;
+    console.log(start);
 
     const arrayCount =
       await prisma.$queryRaw`SELECT Date("createdAt"), count("createdAt") FROM "Report" WHERE "createdAt" > ${new Date(
@@ -54,5 +55,44 @@ init.get('/', async function (req, res) {
     });
   }
 });
+
+init.get(
+  '/detail-quiz/:id',
+  passport.authenticate('jwt', { session: false }),
+  async function (req, res) {
+    try {
+      if (req.user.role !== 'admin') {
+        res.status(403).json({ error: 'fobiddem' });
+        return;
+      }
+      const quiz = await prisma.quiz.findUnique({
+        where: {
+          id: Number(req.params.id),
+        },
+        include: {
+          questions: {
+            include: {
+              answers: true,
+              type: true,
+            },
+          },
+          reports: {
+            include: {
+              reportQuestions: true,
+              players: {
+                include: {
+                  playerAnswers: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      res.json(quiz);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+);
 
 module.exports = init;
